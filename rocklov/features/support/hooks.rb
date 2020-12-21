b7bd14587ce56dbe49c2e9f3628ecab39bde0880
Gustavo Teixeira <iwhrim@gmail.com>
2020-12-20 22:09:36 -0300

    Adding Allure Reports

--- rocklov/features/support/hooks.rb
+++ rocklov/features/support/hooks.rb
@@ -4,4 +4,17 @@ Before do
   @signup_page = SignupPage.new
   @dashboard_page = DashboardPage.new
   @equipments_page = EquipmentsPage.new
+
+  # page.driver.browser.manage.window.maximize
+  page.current_window.resize_to(1440, 900)
+end
+
+After do
+  temp_screenshot = page.save_screenshot("logs/temp_screenshot.png")
+
+  Allure.add_attachment(
+    name: "Screenshot",
+    type: Allure::ContentType::PNG,
+    source: File.open(temp_screenshot),
+  )
 end
